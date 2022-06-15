73d65c751a4accfef01ff327711fe6bdc524b681
elmergustavo <elmergustavo79@gmail.com>
2022-06-15 13:36:43 -0600

    animation button heade ❤️

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,12 +12,28 @@ export const Header = () => {
       justify-between bg-white py-2 px-2 shadow-lg sm:py-3 sm:px-8"
       >
         <div className="flex grow-0 basis-1/3 md:hidden">Menu BTN</div>
-        <div className="flex grow-0 basis-1/3 justify-center md:justify-start">
-          <Logo />
+        <div className="flex grow-0 basis-1/3 justify-center md:justify-start ">
+          <Link href={"/#"}>
+            <a className="my-auto flex w-[140px] md:ml-0 mx-12">
+              <Logo />
+            </a>
+          </Link>
+          <div className="ml-10 hidden gap-2 md:ml-2 md:flex">
+            <Link href="/#" passHref>
+              <Button variant="ghost" as="a" className="hidden md:inline-flex">
+                Option 1
+              </Button>
+            </Link>
+            <Link href="/#" passHref>
+              <Button variant="ghost" as="a" className="hidden md:inline-flex">
+                Option 2
+              </Button>
+            </Link>
+          </div>
         </div>
         <div className="relative flex grow-0 basis-1/3 justify-end gap-2">
           <Link href={"/#"} passHref>
-            <Button as="a" className="hidden md:inline-flex">
+            <Button as="a" className="hidden ">
               For explample
             </Button>
           </Link>
