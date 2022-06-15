d106af9f4b72e3174765570fe1fb4d37ff6cf702
danieltistoj <jose.tisrey@gmail.com>
2022-06-15 12:44:45 -0600

    inicio de componente

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { Hero } from "../components/Hero";
 import { Header } from "../components/Header";
+import { Cards } from "@/components/Cards";
 
 const Home: NextPage = () => {
   return (
@@ -14,6 +15,7 @@ const Home: NextPage = () => {
       <Header />
       <Hero />
       <p className="text-3xl">Home Page</p>
+      <Cards/>
     </>
   );
 };
