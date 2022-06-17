c09f032227684ad562529edf3a12a6860ebab6ac
danieltistoj <jose.tisrey@gmail.com>
2022-06-16 18:36:41 -0600

    Agregar mas tarjetas

--- components/Cards/Cards.tsx
+++ components/Cards/Cards.tsx
@@ -8,11 +8,53 @@ export const Cards = () => {
         {/*---Tarjeta 1---*/}
         <div className="my-20 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
           {/*contenido de la tarjeta*/}
-          <div className="max-w-md mx-auto bg-white rounded-lg shadow-2xl overflow-hidden transform transition duration-500 hover:scale-110">
+          <div className="max-w-md mx-auto bg-white rounded-lg shadow-2xl overflow-hidden transform transition duration-500 hover:scale-110 cursor-pointer">
             {/*Sector de imagen*/}
             <img
               className="w-full"
               src="https://media.istockphoto.com/photos/couple-eating-lunch-with-fresh-salad-and-appetizers-picture-id944478708?s=612x612"
+            />
+            {/*Sector texto*/}
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p>
+            </div>
+          </div>
+        </div>
+        {/*---Fin tarjeta---*/}
+        {/*---Tarjeta 2---*/}
+        <div className="my-20 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+          {/*contenido de la tarjeta*/}
+          <div className="max-w-md mx-auto bg-white rounded-lg shadow-2xl overflow-hidden transform transition duration-500 hover:scale-110 cursor-pointer">
+            {/*Sector de imagen*/}
+            <img
+              className="w-full"
+              src="https://image.shutterstock.com/image-photo/grilled-salmon-vegetables-600w-224518786.jpg"
+            />
+            {/*Sector texto*/}
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p>
+            </div>
+          </div>
+        </div>
+        {/*---Fin tarjeta---*/}
+        {/*---Tarjeta 3---*/}
+        <div className="my-20 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+          {/*contenido de la tarjeta*/}
+          <div className="max-w-md mx-auto bg-white rounded-lg shadow-2xl overflow-hidden transform transition duration-500 hover:scale-110 cursor-pointer">
+            {/*Sector de imagen*/}
+            <img
+              className="w-full"
+              src="https://image.shutterstock.com/image-photo/plate-grilled-chicken-vegetables-isolated-600w-603569441.jpg"
             />
             {/*Sector texto*/}
             <div className="px-6 py-4">
