494797135f28d33f5fe869010dfc273dbacc1487
danieltistoj <jose.tisrey@gmail.com>
2022-06-22 20:54:33 -0600

    inicio de menu

---
+++ components/Menu/Menu.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Menu = () => {
+  return (
+    <div>Menu</div>
+  )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import { CardInfo } from "@/components/Banner/CardInfo";
 import Hero from "@/components/Banner/HeroBanner";
 import { Cards } from "@/components/Cards/Cards";
+import { Menu } from "@/components/Menu/Menu";
 import { Header } from "@/components/Header/Header";
 import Navbar from "../components/Header/Navbar"
 import { ThemeProvider } from "@/context/ThemeContext";
@@ -15,6 +16,7 @@ function Home() {
       <main className="mx-auto max-w-screen-xl px-6 sm:px-8">
         <div className="mt-16 space-y-20">
           <Cards />
+          <Menu/>
           <About />
         </div>
       </main>
