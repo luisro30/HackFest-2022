db104bc42018076bc46fdf85ee34266cb076d626
Daniel-Abac <ddaniel_abac@outlook.es>
2022-06-24 17:44:48 -0600

    Update Footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,25 +1,29 @@
 import React from 'react'
 import Image from 'next/image'
-import img from "../../assets/Github.gif"
+import img from "../../assets/github.png"
 
 export const Footer = () => {
   return (
     <>
-     <footer className=' bg-gradient-to-r from-green-500 to-green-800 flex gap-3 flex-wrap '>
-     <div className='text-center py-5 text-white text-lg'>
-        <p>Si quieres ser colaborador puedes dar click al logo de GitHub</p>
+     <footer className=' bg-gradient-to-r from-green-500 to-green-800 flex flex-row items-center justify-center flex-wrap gap-8'>
+      <div className='w-64 text-center py-5 text-white text-lg'>
+        <p>Si deseas se colaborardor, puedes darle click al icono de GitHub</p>
       </div>
-     <div className='w-64 text-center py-5 text-white text-lg'>
+      <div>
+        <a href="https://github.com/luisro30/HackFest-2022">
+        <div className='w-20'>
+          <Image
+            src={img}
+            alt= "corporate"
+          />
+        </div>
+        </a>
+      </div>
+      <div className='w-64 text-center py-5 text-white text-lg'>
         <p>“Aquellos que piensan que no tienen tiempo para una 
           alimentación saludable tarde o temprano encontrarán 
           tiempo para la enfermedad”</p>
       </div>
-      <div className="w-20 ">
-      <Image
-        src={img}
-        alt= "corporate"
-      />
-     </div>
     </footer>
     </>
   )
